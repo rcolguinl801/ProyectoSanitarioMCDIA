d972b85240db4bb28275f83c22db78cd0fe21302
roberto olguin <rcolguinl@soe.uagrm.edu.bo>
2025-02-25 00:32:11 -0400

    add changes on cama entity and medico entity

---
+++ ProyectoSanitario/Schema/Tables/cama.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [servicios].[cama](
+	[codCama] [int] IDENTITY(1,1) NOT NULL,
+	[codHospital] [int] NOT NULL,
+	[idServicio] [int] NOT NULL,
+	[estado] [bit] DEFAULT 1,
+	[nroHabitacion] [int] DEFAULT 0,
+	PRIMARY KEY(codCama, codHospital, idServicio),
+	CONSTRAINT [FK_Hospital_hosp] FOREIGN KEY (codHospital) REFERENCES [servicios].[hospital_servicio](codHospital),
+	CONSTRAINT [FK_Hospital_serv] FOREIGN KEY (idServicio) REFERENCES [servicios].[hospital_servicio](idServicio)
+););
---
+++ ProyectoSanitario/Schema/Tables/medico.sql
@@ -0,0 +1,9 @@
+﻿CREATE TABLE [servicios].[medico](
+	[codMedico]  [int] IDENTITY(1,1) NOT NULL,
+	[dni] [int] NOT NULL,
+	[apPaterno] [varchar](200) NULL,
+	[apMaterno] [varchar](200) NULL,
+	[nombres] [varchar](200) NULL,
+	[fechaNacimiento] [date],
+	PRIMARY KEY(codMedico)
+);
