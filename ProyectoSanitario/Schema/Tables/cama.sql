e7dc9c0b8c571d7e3c173a8c8e88d429f1aa5fad
roberto olguin <rcolguinl@soe.uagrm.edu.bo>
2025-02-25 01:08:20 -0400

    update changes on cama entity and medico entity

--- ProyectoSanitario/Schema/Tables/cama.sql
+++ ProyectoSanitario/Schema/Tables/cama.sql
@@ -1,4 +1,6 @@
-﻿CREATE TABLE [servicios].[cama](
+﻿SET QUOTED_IDENTIFIER ON
+GO
+CREATE TABLE [servicios].[cama](
 	[codCama] [int] IDENTITY(1,1) NOT NULL,
 	[codHospital] [int] NOT NULL,
 	[idServicio] [int] NOT NULL,
@@ -7,4 +9,5 @@
 	PRIMARY KEY(codCama, codHospital, idServicio),
 	CONSTRAINT [FK_Hospital_hosp] FOREIGN KEY (codHospital) REFERENCES [servicios].[hospital_servicio](codHospital),
 	CONSTRAINT [FK_Hospital_serv] FOREIGN KEY (idServicio) REFERENCES [servicios].[hospital_servicio](idServicio)
-););
+)
+GO
--- ProyectoSanitario/Schema/Tables/medico.sql
+++ ProyectoSanitario/Schema/Tables/medico.sql
@@ -1,4 +1,7 @@
-﻿CREATE TABLE [servicios].[medico](
+﻿SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [servicios].[medico](
 	[codMedico]  [int] IDENTITY(1,1) NOT NULL,
 	[dni] [int] NOT NULL,
 	[apPaterno] [varchar](200) NULL,
@@ -6,4 +9,5 @@
 	[nombres] [varchar](200) NULL,
 	[fechaNacimiento] [date],
 	PRIMARY KEY(codMedico)
-);
+)
+GO
